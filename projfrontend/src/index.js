626bbb2565ca0e039ac787f8b8a3ac4930f02bfd
shahbaz-cse <cseshahbaz@gmail.com>
2022-01-19 03:10:48 +0530

    Added Signup Page

--- projfrontend/src/index.js
+++ projfrontend/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import Routes from "./Routes";
+import ReactDOM from "react-dom";
 
-
-ReactDOM.render(<Routes/>, document.getElementById("root"));
+ReactDOM.render(<Routes />, document.getElementById("root"));
